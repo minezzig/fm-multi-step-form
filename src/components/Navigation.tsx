52156e843480ac4ff6d7580b29e66b9621949247
Greg <gregoryminezzi@gmail.com>
2024-12-03 14:05:15 +0100

    change mobile nav padding

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,7 +21,7 @@ const {order} = useOrder();
   };
 
   return (
-    <div className="fixed bottom-0 left-0 md:mt-auto flex w-full items-center justify-between bg-neutral5 p-5 md:static md:mb-3 md:p-0 md:pb-0">
+    <div className="fixed bottom-0 left-0 md:mt-auto flex w-full items-center justify-between bg-neutral5 p-3 md:p-5 md:static md:mb-3 md:p-0 md:pb-0">
       {step > 1 && step < 5 && (
         <Button
           text={"Go Back"}
